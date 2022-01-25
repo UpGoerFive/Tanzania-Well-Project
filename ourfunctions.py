98e63778e3ea5b47d55eb9c88eaead69c30091a1
UpGoerFive <59941301+UpGoerFive@users.noreply.github.com>
2022-01-25 13:38:13 -0600

    Fixed default preprocessor

--- ourfunctions.py
+++ ourfunctions.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import logging
-from sklearnex import patch_sklearn
-patch_sklearn()
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split, cross_val_score
+# Current version of sklearn is still too old I think, might try to upgrade to use below option
+# from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import train_test_split, cross_val_score, RandomizedSearchCV# ,  HalvingRandomSearchCV
 from sklearn.compose import ColumnTransformer, make_column_selector
 
 #########################Valeria###########################
@@ -48,20 +48,18 @@ class Modeler:
     {'classifier': <classifier>,
      'preprocessor': <preprocessor>}
 
-    preprocessor can be None if the default preprocessor is acceptable(Need to implement). This class also
+    preprocessor can be None if the default preprocessor is acceptable. This class also
     logs model output to a default model-run.log file. Each train or test method also has an optional print
     keyword argument that will print output if desired, as well as log it to the output file. This defaults
     to True for single runs, and False for multiple runs.
-
-    When instantiating the class, if the prep(default preprocessor) argument is left out,
-    the Modeler will generate a default one from it's input data, so X and y must be given
-    in that instance.
     """
-    def __init__(self, models={}, prep=None, X=pd.DataFrame(), y=pd.DataFrame(), log='model-run.log'):
+    def __init__(self, models={}, X=pd.DataFrame(), y=pd.DataFrame()):
         self._models=models
-        self._preprocessor=prep
+        self._tuning = {}
 
         for name in self._models:
+            if not self._models[name]['preprocessor']:
+                self._models[name]['preprocessor'] = self.create_default_prep()
             self._models[name]['output'] = None
             self._models[name]['fit_classifier'] = None
             self._models[name]['time_ran'] = None
@@ -71,36 +69,46 @@ class Modeler:
         else:
             self._X_train, self._X_test, self._y_train, self._y_test = None, None, None, None
 
-        if not prep:
-            self._preprocessor = self.create_default_prep(X)
-            
-    def create_default_prep(self, X):
+    def create_default_prep(self):
+        def to_object(x):
+            return pd.DataFrame(x).astype(str)
+
+        fun_tr = FunctionTransformer(to_object)
+
         numeric_transformer = Pipeline(
             steps=[('imputer', SimpleImputer(strategy='median'))]
         )
 
         categorical_transformer = Pipeline(
-            steps=[('imputer', SimpleImputer(strategy='constant', fill_value='Missing'))]
+            steps=[('imputer', SimpleImputer(strategy='constant', fill_value='Missing')),
+                ('casting', fun_tr),
+                ('one_hot_encode', OneHotEncoder(handle_unknown='ignore'))]
         )
-
         preprocessor = ColumnTransformer(
-            transformers=[
-                ("numeric", numeric_transformer, make_column_selector(dtype_include=np.number)),
-                ("categorical", categorical_transformer, make_column_selector(dtype_exclude=np.number))
-            ]
-        )
+                            transformers=[
+                                ("numeric", numeric_transformer, make_column_selector(dtype_include=np.number)),
+                                ("categorical", categorical_transformer, make_column_selector(dtype_exclude=np.number))
+                            ]
+                        )
 
         return preprocessor
 
     def add_model(self, name, model):
         """
         Basic mechanism to add a model, model should provide classifier and preprocessor fields.
-        To Do: implement default preprocessor assignment.
+        Model can have None as the preprocessor, in which case a default will be provided, however
+        the 'preprocessor' key must still be provided.
         """
+        if not model['preprocessor']:
+                model['preprocessor'] = self.create_default_prep()
+
         self._models[name] = model
         self._models[name]['output'] = None
         self._models[name]['fit_classifier'] = None
         self._models[name]['time_ran'] = None
+
+    def remove_model(self, name):
+        del self._models[name]
 
     def change_prep(self, name, prep):
         """
@@ -115,11 +123,11 @@ class Modeler:
         """
         print(f"{name}: {self._models[name]}")
 
-    def train_model(self, name, X_train=pd.DataFrame(), y_train=pd.DataFrame(), print=True):
+    def train_model(self, name, X_train=pd.DataFrame(), y_train=pd.DataFrame(), print=True, cv_only=False):
         """
         Train a single model. Fits all preprocessing transformers for later testing.
-        Records and outputs cross validate scores, but also trains the classifier
-        for later testing. Optional printing ability.
+        Records and outputs cross validate scores. The cv_only option determines if the method will 
+        fit a classifier, which is required before testing. Optional printing ability.
         """
         if print:
             logger.addHandler(c_handler)
@@ -127,13 +135,14 @@ class Modeler:
         if X_train.empty:
             X_train = self._X_train
         if y_train.empty:
-            y_train = self._y_train
+            y_train = np.array(self._y_train).ravel()
         model = self._models[name]
 
         X_train_processed = model['preprocessor'].fit_transform(X_train)
 
-        model['fit_classifier'] = model['classifier'].fit(X_train_processed, y_train)
-        logger.info(f"{name} has been fit.")
+        if not cv_only:
+            model['fit_classifier'] = model['classifier'].fit(X_train_processed, y_train)
+            logger.info(f"{name} has been fit.")
 
         model['output'] = cross_val_score(
             estimator=model['classifier'],
@@ -145,14 +154,12 @@ class Modeler:
         if print:
             logger.removeHandler(c_handler)
 
-    def train_all(self, X_train=pd.DataFrame(), y_train=pd.DataFrame(), print=False):
+    def train_all(self, X_train=pd.DataFrame(), y_train=pd.DataFrame(), print=False, cv_only=False):
         """
         Train all available models. Fits all preprocessing transformers for later testing.
-        Records and outputs cross validate scores, but also trains the classifier
-        for later testing. Optional printing ability.
+        Records and outputs cross validate scores. The cv_only option determines if the method will 
+        fit a classifier, which is required before testing. Optional printing ability.
         """
-        if print:
-            logger.addHandler(c_handler)
 
         if X_train.empty:
             X_train = self._X_train
@@ -160,10 +167,7 @@ class Modeler:
             y_train = self._y_train
 
         for model in self._models:
-            self.train_model(model, X_train, y_train, print)
-
-        if print:
-            logger.removeHandler(c_handler)
+            self.train_model(model, X_train, y_train, print, cv_only)
 
     def test_model(self, name, X_test=pd.DataFrame(), y_test=pd.DataFrame(), print=True):
         """
@@ -176,7 +180,7 @@ class Modeler:
         if X_test.empty:
             X_test = self._X_test
         if y_test.empty:
-            y_test = self._y_test
+            y_test = np.array(self._y_test).ravel()
         model = self._models[name]
 
         X_test_processed = model['preprocessor'].transform(X_test)
@@ -195,8 +199,6 @@ class Modeler:
         Test all available models. Uses already fitted preprocessor pipelines and classifiers.
         Raises an exception if there is no fit classifier for a model. Optional printing.
         """
-        if print:
-            logger.addHandler(c_handler)
 
         if X_test.empty:
             X_test = self._X_test
@@ -206,8 +208,19 @@ class Modeler:
         for model in self._models:
             self.test_model(model, X_test, y_test, print)
 
+    def hyper_search(self, name, searcher=RandomizedSearchCV, params=None, print=False):
+        """
+        Hyper parameter tuning function, defaults to RandomizedSearchCV, but any search function
+        you want can be passed in.
+        """
+        if print:
+            logger.addHandler(c_handler)
+
+        
+
         if print:
             logger.removeHandler(c_handler)
+
 
     def plot_models(self):
         """Skylar slide style."""
