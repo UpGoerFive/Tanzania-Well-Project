ee52c6040e1fc5c71f4532b155d90af7214859cf
UpGoerFive <59941301+UpGoerFive@users.noreply.github.com>
2022-01-24 20:07:18 -0600

    Logging finally works for class. Includes basicKNN

--- ourfunctions.py
+++ ourfunctions.py
@@ -19,24 +19,26 @@ from sklearn.compose import ColumnTransformer
 
 
 #########################Nathaniel#########################
-# logger = logging.getLogger()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
-# # Create handlers
-# c_handler = logging.StreamHandler()
-# f_handler = logging.FileHandler('model-run.log')
-# c_handler.setLevel(logging.DEBUG)
-# f_handler.setLevel(logging.DEBUG)
+# Create handlers
+c_handler = logging.StreamHandler()
+f_handler = logging.FileHandler('model-run.log')
+c_handler.setLevel(logging.DEBUG)
+f_handler.setLevel(logging.DEBUG)
 
-# # Create formatters and add it to handlers
-# c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-# f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# c_handler.setFormatter(c_format)
-# f_handler.setFormatter(f_format)
+# Create formatters and add it to handlers
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
 
-# # Add handlers to the logger
+# Add handlers to the logger
 # logger.addHandler(c_handler)
-# logger.addHandler(f_handler)
-# logger.info("Logger set up")
+logger.addHandler(f_handler)
+logger.info("Logger set up")
+
 
 class Modeler:
     """
@@ -47,7 +49,9 @@ class Modeler:
      'preprocessor': <preprocessor>}
 
     preprocessor can be None if the default preprocessor is acceptable(Need to implement). This class also
-    logs model output to a default model-run.log file.
+    logs model output to a default model-run.log file. Each train or test method also has an optional print
+    keyword argument that will print output if desired, as well as log it to the output file. This defaults
+    to True for single runs, and False for multiple runs.
 
     When instantiating the class, if the prep(default preprocessor) argument is left out,
     the Modeler will generate a default one from it's input data, so X and y must be given
@@ -115,6 +119,13 @@ class Modeler:
         print(f"{name}: {self._models[name]}")
 
     def train_model(self, name, X_train=pd.DataFrame(), y_train=pd.DataFrame(), print=True):
+        """
+        Train a single model. Fits all preprocessing transformers for later testing.
+        Records and outputs cross validate scores, but also trains the classifier
+        for later testing. Optional printing ability.
+        """
+        if print:
+            logger.addHandler(c_handler)
 
         if X_train.empty:
             X_train = self._X_train
@@ -125,25 +136,46 @@ class Modeler:
         X_train_processed = model['preprocessor'].fit_transform(X_train)
 
         model['fit_classifier'] = model['classifier'].fit(X_train_processed, y_train)
-        #logger.info(f"{name} has been fit.")
+        logger.info(f"{name} has been fit.")
 
         model['output'] = cross_val_score(
             estimator=model['classifier'],
             X=X_train_processed,
             y=y_train
         )
-        #logger.info(f"Cross validate scores for {name}: {model['output']}")
+        logger.info(f"Cross validate scores for {name}: {model['output']}")
+
+        if print:
+            logger.removeHandler(c_handler)
 
     def train_all(self, X_train=pd.DataFrame(), y_train=pd.DataFrame(), print=False):
+        """
+        Train all available models. Fits all preprocessing transformers for later testing.
+        Records and outputs cross validate scores, but also trains the classifier
+        for later testing. Optional printing ability.
+        """
+        if print:
+            logger.addHandler(c_handler)
+
         if X_train.empty:
             X_train = self._X_train
         if y_train.empty:
             y_train = self._y_train
 
         for model in self._models:
-            self.train_model(model, X_train, y_train)
+            self.train_model(model, X_train, y_train, print)
+
+        if print:
+            logger.removeHandler(c_handler)
 
     def test_model(self, name, X_test=pd.DataFrame(), y_test=pd.DataFrame(), print=True):
+        """
+        Test a single model. Uses already fitted preprocessor pipeline and classifier.
+        Raises an exception if there is no fit classifier for the model. Optional printing.
+        """
+        if print:
+            logger.addHandler(c_handler)
+
         if X_test.empty:
             X_test = self._X_test
         if y_test.empty:
@@ -156,16 +188,29 @@ class Modeler:
             raise Exception("This model has not been fit yet.")
 
         model['test_output'] = model['fit_classifier'].score(X_test_processed, y_test)
-        #logger.info(f"{name} test score: {model['test_output']}")
+        logger.info(f"{name} test score: {model['test_output']}")
+
+        if print:
+            logger.removeHandler(c_handler)
 
     def test_all(self, X_test=pd.DataFrame(), y_test=pd.DataFrame(), print=False):
+        """
+        Test all available models. Uses already fitted preprocessor pipelines and classifiers.
+        Raises an exception if there is no fit classifier for a model. Optional printing.
+        """
+        if print:
+            logger.addHandler(c_handler)
+
         if X_test.empty:
             X_test = self._X_test
         if y_test.empty:
             y_test = self._y_test
 
         for model in self._models:
-            self.test_model(model, X_test, y_test)
+            self.test_model(model, X_test, y_test, print)
+
+        if print:
+            logger.removeHandler(c_handler)
 
     def plot_models(self):
         """Skylar slide style."""
